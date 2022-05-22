991600c82eb795adcf3700ce7245861df5b66068
joon <sh05158@flysher.net>
2022-05-23 06:19:45 +0900

    commit dbproject

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
+# -*- coding: utf-8 -*- 
+
 import sys
 from datetime import datetime
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+
 #CREATE TABLE tableName(tableName CHAR(50) grade CHAR(3) sentence VARCHAR(100) department VARCHAR(20))
 #INSERT into tableName values("minjoon","A+","hello world this is minjoon","SW")
 #SELECT * FROM tableName WHERE name = "minjoon" AND grade = "A+" OR department = "SW"
 #SELECT name grade FROM tableName
 
-def error(errstr):
+def err(errstr):
     print("Exception ocurred : ",errstr)
+    exit()
 
 def processQuery(query):
     print(query)
-    createTable("a","a")
+    # createTable("a","a")
+    insertRow("a","a")
     
+def createDirectory(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        err("Error: Failed to create the directory.")
+
+
+
 def createTable(tableName, args):
     tableName = "testTable"
     args = [("name","CHAR(50)"),("grade","CHAR(3)"),("sentence","VARCHAR(100)"),("department","VARCHAR(20)")]
     
-    #메타 파일 열자
+    #메타메타 파일 열자
     
     try:
-        meta = open("./meta.meta",'r')
+        meta = open("./meta.meta", 'r+')
     except:
         meta = open("./meta.meta","w")
         meta.writelines("JOON DB meta.meta ["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]")
@@ -27,9 +44,71 @@ def createTable(tableName, args):
         meta.writelines("\n")
         meta.flush()
         meta.close()
-        meta = open("./meta.meta",'r')
+        meta = open("./meta.meta",'r+')
 
-    print(meta.read())
+    while True:
+        readData = meta.readline()
+
+        if readData != "":
+            currTableName = readData.split(".meta")[0]
+            print(currTableName)
+            if tableName == currTableName:
+                err("tableName already exists")
+        else:
+            break
+
+    meta.writelines("\n"+tableName+".meta ./"+tableName+"/")
+    meta.flush()
+    meta.close()
+
+
+
+    #테이블 폴더 만들자
+    createDirectory("./"+tableName)
+    #메타 만들자
+
+    meta = open("./"+tableName+"/"+tableName+".meta","a+")
+    meta.writelines(tableName+".meta ["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]")
+    meta.writelines("\n")
+    meta.writelines("\n")
+
+    for arg in args:
+        meta.writelines("\n"+arg[0]+" "+arg[1])
+
+def insertRow(tableName, args):
+    tableName = "testTable"
+    args = ["minjoon", "A+", "hello world this is minjoon", "SW"]
+
+    
+    #메타에 쓰이는 3종 세트 
+    cols = []
+    types = []
+    sizes = []
+
+    #우선 테이블 메타 파일 열어야됌
+
+    try:
+        reader = open("./"+tableName+"/"+tableName+".meta","r")
+    except:
+        err(tableName+" meta not exists")
+
+    while True:
+        readData = reader.readline()
+
+        if " CHAR(" in readData or " VARCHAR(" in readData:
+            cols.append(readData.split(' ')[0])
+            types.append(readData.split(' ')[1].split('(')[0])
+            sizes.append(int(readData.split(' ')[1].split('(')[1].split(')')[0]))
+
+        if readData=="":
+            break 
+
+
+    print(cols)
+    print(types)
+    print(sizes)
+            
+    
     
 if __name__ != "__main__":
     exit()
